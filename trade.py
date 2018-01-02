0342509e2fe3263efbf9a0fd76b8e1afbaa74e36
Zane Bilous <zanebilous@gmail.com>
2018-01-02 01:24:46 -0500

    Rewrite entire bot

--- trade.py
+++ trade.py
@@ -10,39 +10,42 @@ SELL_VOLUME = 0.3 # percent of volume to sell
 RUN_INTERVAL = 120 # in minutes
 
 
-# Returns a dictionary w/ the prices and percent changes of all cryptos in the WATCHING list
+# Returns the total free balance of the ticker in the account.
+def get_ticker_balance(ticker):
+    return float(b.get_asset_balance(asset=ticker)['free'])
+
+
+# Returns the price and 24hr change for the ticker.
+def get_ticker_data(ticker):
+    data = b.get_ticker(symbol=ticker)
+    return {'price': float(data['lastPrice']), 'change': float(data['priceChangePercent'])}
+
+
+# Returns a dictionary w/ the prices and percent changes of all cryptos in WATCHING.
 def get_watching_data():
-    prices = {}
-    for ticker in WATCHING:
-        data = b.get_ticker(symbol=ticker)
-        prices[ticker] = {}
-        prices[ticker]['price'] = float(data['lastPrice'])
-        prices[ticker]['change'] = float(data['priceChangePercent'])
-    return prices
+    return {ticker: get_ticker_data(ticker) for ticker in WATCHING}
 
 
-high = lambda x, y: x > y # compare functions for get_greatest_change
-low = lambda x, y: x < y
-# Returns a list containing the ticker with the highest change (high or low),
-# the change, and the last price.
-def get_greatest_change(comp=high):
-    data = get_watching_data()
-    greatest = {}
+# Returns a tuple containing (lowest, highest) 24hr percent changes among the cryptos in WATCHING.
+def get_lowest_highest(data):
+    low, high = 0
     for ticker in data:
         change = data[ticker]['change']
-        if comp(change, greatest[1]):
-            greatest['ticker'] = ticker
-            greatest['change'] = change
-            greatest['price'] = data[ticker]['price']
-    return greatest
+        if change > high:
+            high = change
+            highest = ticker
+        elif change < low:
+            low = change
+            lowest = ticker
+    return (lowest, highest)
 
 
-# Places a market sell order for the crypto with the highest 24hr increase.
-def sell_highest():
-    highest = get_greatest_change()
+# Places a market sell order the crypto. Uses the SELL_VOLUME constant
+# to determine the volume to sell.
+def sell(ticker):
     b.order_market_sell(
-        symbol=highest['ticker'],
-        quantity=SELL_VOLUME * b.get_asset_balance(asset=greatest['ticker'][0:-3])) # truncate ETH
+        symbol=ticker,
+        quantity=SELL_VOLUME * get_ticker_balance(ticker[0:-3]))
 
 
 # Places a market buy order for the crypto with the highest 24hr decrease.
@@ -52,14 +55,12 @@ def buy_lowest():
        symbol=lowest['ticker'],
        # buy with 97% ETH balance because we sell highest into ETH first
        # and we don't want rounding errors to not let the order go through
-       quantity=b.get_asset_balance(asset='ETH') * 0.97 / lowest['price'])
+       quantity=get_ticker_balance('ETH') * 0.97 / lowest['price'])
 
 
 # Places a market buy order for the ticker using the specified amount of USD in ether.
-def buy(ticker, usd):
-    eth_usd = b.get_ticker(symbol='ETHUSDT')['lastPrice']
-    vol_eth = eth_usd * usd
-    vol_ticker = vol_eth / b.get_ticker(symbol=ticker)['lastPrice']
+def buy(ticker, price, eth):
+    vol_ticker = eth / price
     try:
         b.order_market_buy(
             symbol=ticker,
@@ -70,21 +71,23 @@ def buy(ticker, usd):
 
 
 # Places a market buy order for each of the cryptos in WATCHING for the specified amount of USD.
-def buy_watching(usd):
-    for ticker in WATCHING:
-        buy(ticker, usd/len(WATCHING))
-
+def buy_watching(data):
+    eth_per = get_ticker_balance('ETH') * 0.97 / len(watching)
+    for ticker in data:
+        buy(ticker, ticker['price'], eth_per)
 
 
 def run():
+    data = get_watching_data()
+    lowest, highest = get_lowest_highest()
     try:
-        sell_highest()
+        sell(lowest)
     except (BinanceAPIException, BinanceOrderException) as e:
         print(e)
         logging.exception("Sell order failed:")
         return
     try:
-        buy_lowest()
+        buy(highest, data[highest]['price'], get_asset_balance('ETH'))
     except (BinanceAPIException, BinanceOrderException) as e:
         print(e)
         logging.exception("Buy order failed:")

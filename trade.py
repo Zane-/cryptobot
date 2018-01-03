f7cadacd776e28a0ab922071aa6a76b301f3ea3c
Zane Bilous <zanebilous@gmail.com>
2018-01-02 21:01:33 -0500

    Add transaction csv

--- trade.py
+++ trade.py
@@ -1,12 +1,16 @@
+import csv
 import logging
+from collections import deque
+from datetime import datetime
 from math import floor
+
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 from auth import *
 
 logging.basicConfig(level=logging.DEBUG, filename='bot.log')
 
 WATCHING = ['ICXETH', 'TRXETH', 'XLMETH', 'ADAETH', 'IOTAETH', 'XRPETH', 'NAVETH', 'XVGETH']
-SELL_VOLUME = 0.3 # percent of volume to sell
+SELL_VOLUME = 0.3 # percent of volume to sell in the run function
 
 
 # Returns the total free balance of the ticker in the account.
@@ -25,7 +29,8 @@ def get_watching_data():
     return {ticker: get_ticker_data(ticker) for ticker in WATCHING}
 
 
-# Returns a tuple containing (lowest, highest) 24hr percent changes among the cryptos in WATCHING.
+# Returns a tuple containing (lowest, highest) 24hr percent changes
+# among the cryptos in WATCHING.
 def get_lowest_highest(data):
     low, high = 0, 0
     for ticker in data:
@@ -37,8 +42,8 @@ def get_lowest_highest(data):
     return (lowest, highest)
 
 
-# Places a market sell order the crypto. Uses the SELL_VOLUME constant
-# to determine the volume to sell.
+# Places a market sell order for the ticker using the
+# given percentage of the available balance.
 def sell(ticker, percent):
     b.order_market_sell(
         symbol=ticker,
@@ -46,6 +51,7 @@ def sell(ticker, percent):
 
 
 # Places a market buy order for the ticker using the specified amount of USD in ether.
+# The price of the crypto is given to determine the volume to buy.
 def buy(ticker, price, eth):
     vol_ticker = floor(eth / price)
     b.order_market_buy(
@@ -53,8 +59,8 @@ def buy(ticker, price, eth):
         quantity=floor(vol_ticker * 0.97)) # 97% for fees and price fluctuations
 
 
-# Places a market buy order for each of the cryptos in WATCHING for the specified amount of USD.
-# Used to initially load bot.
+# Places a market buy order for each of the cryptos in WATCHING
+# for the specified amount of USD. Used to initially load bot.
 def buy_watching(data):
     eth_per = get_ticker_balance('ETH') * 0.97 / len(WATCHING)
     for ticker in data:
@@ -64,8 +70,9 @@ def buy_watching(data):
             print(e)
             logging.exception("Buy order failed:")
 
-#Places a market sell order for the full amount of each of the cryptos in WATCHING
-#Used to redistrubute total funds into all cryptos when distribution becomes too skewed
+
+# Places a market sell order for the full amount of each of the cryptos in WATCHING.
+# Used to redistrubute total funds into all cryptos when distribution becomes too skewed.
 def redistribute_funds(data):
     for ticker in data:
         try:
@@ -73,39 +80,74 @@ def redistribute_funds(data):
         except (BinanceAPIException, BinanceOrderException) as e:
             print(e)
             logging.exception("Redistribution failed:")
-    #error catching done in buy_watching()
+    # error catching done in buy_watching()
     buy_watching(data)
 
 
-def run():
+# Returns the equivalent USD amount of all cryptos in the account.
+def get_portfolio(data):
+    eth_usd = get_ticker_data('ETHUSDT')['price']
+    total = eth_usd * get_ticker_balance('ETH')
+    for ticker in data:
+        total += eth_usd * get_ticker_balance(ticker[0:-3]) * data[ticker]['price']
+    return total
+
+
+# Returns the portfolio percent change from the last transaction using the transactions csv.
+def get_portfolio_change(data):
+    last_portfolio = float(get_last_row('transactions.csv')[9])
+    return round(get_portfolio(data) / last_portfolio, 4)
+
+
+# Returns the last row of the filename
+def get_last_row(filename):
+    with open(filename) as f:
+        return deque(csv.reader(f), 1)[0]
+
+
+# Sells the two highest percent change cryptos and the buys the two lowest.
+def main():
     data = get_watching_data()
     lowest, highest = get_lowest_highest(data)
-    lowest_data = data.pop(lowest, None)
-    highest_data = data.pop(highest, None)
-    second_lowest, second_highest = get_lowest_highest(data)
+    data_copy = data.copy()
+    data_copy.pop(lowest, None)
+    data_copy.pop(highest, None)
+    second_lowest, second_highest = get_lowest_highest(data_copy)
+
     try:
         sell(highest, SELL_VOLUME)
-        print('Sold {} for {} at a change of {}'.format(highest, highest_data['price'], highest_data['change']))
         sell(second_highest, SELL_VOLUME)
-        print('Sold {} for {} at a change of {}'.format(second_highest, data[second_highest]['price'], data[second_highest]['change']))
     except (BinanceAPIException, BinanceOrderException) as e:
         print(e)
         logging.exception("Sell order failed:")
         return # do not proceed with buy because ETH balance did not get filled
     try:
         eth_per = get_ticker_balance('ETH') / 2 * 0.98
-        buy(lowest, lowest_data['price'], eth_per)
-        print('Bought {} for {} at a change of {}'.format(lowest, lowest_data['price'], lowest_data['change']))
-        buy(lowest, data[second_lowest]['price'], eth_per)
-        print('Bought {} for {} at a change of {}'.format(lowest, data[second_lowest]['price'], data[second_lowest]['change']))
+        buy(lowest, data[lowest]['price'], eth_per)
+        buy(second_lowest, data[second_lowest]['price'], eth_per)
     except (BinanceAPIException, BinanceOrderException) as e:
         print(e)
         logging.exception("Buy order failed:")
 
+    # write data to CSV
+    row = (
+        str(datetime.now()),
+        lowest,
+        data[lowest]['change'],
+        second_lowest,
+        data[second_lowest]['change'],
+        second_highest,
+        data[second_highest]['change'],
+        highest,
+        data[highest]['change'],
+        round(get_portfolio(data), 2),
+        get_portfolio_change(data)
+    )
+
+    with open('transactions.csv', 'a') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(row)
+
 
 if __name__ == '__main__':
-    run()
-
-# TODO :
-# Add logging of all transactions in an easy-to-get-data-from format (csv)
-# Add checks to not trade if the
+    main()

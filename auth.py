91f098aab8828edf57d48a18bf547e71c2894d7a
Zane Bilous <zanebilous@gmail.com>
2018-01-04 17:00:04 -0500

    Restructure code

--- auth.py
+++ auth.py
@@ -8,7 +8,7 @@ with open('api_keys.json') as f:
 #with open('addresses.json') as f:
 #    addresses = json.load(f)
 
-binance = ccxt.binance({
+exchange = ccxt.binance({
     'apiKey': keys['binance']['api_key'],
     'secret': keys['binance']['api_secret'],
 })

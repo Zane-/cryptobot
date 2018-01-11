887c0953f84ed156fe202a41a9e35648df364539
Zane Bilous <zanebilous@gmail.com>
2018-01-10 19:23:11 -0500

    Exchange config unnecessary

--- auth.py
+++ auth.py
@@ -14,7 +14,4 @@ exchange = ccxt.binance({
 })
 
 exchange.load_markets()
-
-with open(f'{exchange.name.lower()}.json') as f:
-    exchange_config = json.load(f)
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####

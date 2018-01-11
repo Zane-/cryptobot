76be0ec68e697980712f094521706b4e7999945e
Zane Bilous <zanebilous@gmail.com>
2018-01-11 14:55:17 -0500

    Update bot file

--- poolbot.py
+++ poolbot.py
@@ -2,12 +2,19 @@ from strategies import PoolProfitBot
 
 def main():
     INTERVAL = 60 * 60 # 60 minutes
-    FEEDERS = ['TRX', 'XVG'] # list shitcoins you're invested in here
+    FEEDERS = { # short term positions here, with initial investments and sell points
+        'TRX': {
+            'initial': 100,
+            'sell_point': 115
+        },
+        'ADA': {
+            'initial': 100,
+            'sell_point': 130
+        }
+    }
     POOL = 'XLM' # long term position here
-    PAIR = 'BTC' # pair to buy/sell things with
-    BASE_AMOUNT = 10 # base investment in USD
-    PROFIT_THRESHOLD = 2 # amount in USD to beconsidered profit, profit gets sold into pool
-    bot = PoolProfitBot(INTERVAL, POOL, FEEDERS, PAIR, BASE_AMOUNT, PROFIT_THRESHOLD)
+    PAIR = 'ETH' # pair to buy/sell things with
+    bot = PoolProfitBot(INTERVAL, FEEDERS, POOL, PAIR)
     bot.start()
 
 if __name__ == '__main__':
